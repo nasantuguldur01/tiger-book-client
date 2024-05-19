bb9011d81483d69fc981ba41d8e11c8d7bbba262
nasantuguldur01 <anasantuguldur@gmail.com>
2024-05-19 22:27:35 +0800

    first commit

---
+++ src/pages/Home/Banner.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import BannerCard from '../shared/BannerCard'
+
+export const Banner = () => {
+    return (
+        <div className=' bg-white  px-4 lg:px-24 flex items-center'>
+            <div className='flex flex-col md:flex-row-reverse justify-between items-center gap-12 py-40'>
+                {/* right side */}
+                <div className='md:w-1/2 h-full'>
+                    <BannerCard />
+                </div>
+
+                {/* left side */}
+                <div className='md:w-1/2 space-y-8'>
+                    <h1 className='lg:text-6xl text-5xl font-bold text-black mb-5 lg:leading-tight leading-snug'>Монгол модон барын бүтээлүүдийг <span className='text-blue-700'> ангилан , бүртгэх систем</span></h1>
+                    <p> Одоогоор Монгол улсын хэмжээнд хадгалагдаж буй модон баран бүтээлүүдийг ангилж ялган бүртгэх нэгдсэн систем хараахан байдаггүй бөгөөд уг системийг хэрэгжүүлснээр Монгол улсын хэмжээнд хадгалагдаж буй модон баран бүтээлүүдийн нэгдсэн сан үүсгэж   үүн дээр тулгуурлан ном зохиолуудыг ангилах 
+,судалгааны эргэлтэд оруулах , нэгдсэн сан үүсгэж салбар дундын судалгааг өргөжүүлэхэд ашиглах боломжтой .</p>
+                    <div>
+                        <input type="search" placeholder='Search a book here' className='py-2 px-2 rounded-s-sm' />
+                        <button className='bg-blue-700 px-6 py-2 text-white font-medium hover:bg-black transition-all ease-in duration-200'>Search</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/Login.jsx
@@ -0,0 +1,91 @@
+import React, { useContext, useState } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthProvider';
+import googleLogo from '../assets/google-logo.svg'
+import fbLogo from '../assets/facebook-log.svg'
+
+export default function Login() {
+
+    const [ErrorMessage, setErrorMessage] = useState('');
+
+    const { signUpWithGmail, login } = useContext(AuthContext);
+
+    console.log(signUpWithGmail)
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const from = location.state?.from?.pathname || '/';
+
+    // login with google
+    const handleRegister = () => {
+        signUpWithGmail().then((result) => {
+            const user = result.user;
+            navigate(from, { replace: true });
+        }).catch((error) => console.log(error))
+    }
+
+    // login with email password
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        // console.log(email, password);
+        login(email, password).then((result) => {
+            // Signed in 
+            const user = result.user;
+            console.log(user);
+            alert("Login successful!")
+            navigate(from, { replace: true });
+            // ...
+        })
+            .catch((error) => {
+                const errorMessage = error.message;
+                setErrorMessage(errorMessage)
+            });
+    }
+
+    return (
+
+        <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+            <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+                <div
+                    className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+                </div>
+                <div className="relative px-10 py-20 bg-white shadow-lg sm:rounded-3xl sm:p-10">
+                    <div className="max-w-md mx-auto">
+                        <div className="flex flex-col items-center justify-center">
+                        <img src={"src/assets/media/logo.jpg" } alt="mongolain tiger work"  className="w-40 h-20"/>
+                        </div>
+                        <div className="divide-y divide-gray-200">
+                            <form onSubmit={handleLogin} className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                                <div className="relative">
+                                    <input id="email" name="email" type="text" className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" required />
+                                </div>
+                                <div className="relative ">
+                                    <input id="password" name="password" type="password" className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" required />
+                                </div>
+                                <div className='font-family: "EB Garamond", serif'>
+                                    <p>{ErrorMessage ? <span className='text-blue-500 text-sm'>Имэйл эсвэл хэрэглэгчийн нэр буруу байна</span> : ''}</p>
+                                    <p className='text-base mt-1'>Шинээр бүртгүүлэх <Link to='/create-user' className='underline text-blue-600'>Бүртгүүлэх</Link></p>
+
+                                </div>
+                                <div className="relative flex items-center justify-center ">
+                                    <button type='submit' className="bg-blue-500 text-white rounded px-6 py-1" >Нэвтрэх </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                    {/* social login */}
+                    <div>
+                        <hr />
+                        <div className="flex w-full items-center flex-col mt-5 gap-3">
+                            <button onClick={handleRegister} className='block'> <img src={googleLogo} alt="" className='w-12 h-12 inline-block' />Google хаягаар нэвтрэх </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
